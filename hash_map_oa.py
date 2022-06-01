23cbc21ba7a45bd4cccaba6bfa3dd4a077a65595
ZChaloner <zchaloner1107@gmail.com>
2022-05-31 22:06:13 -0400

    OA a few methods in

--- hash_map_oa.py
+++ hash_map_oa.py
@@ -119,7 +119,7 @@ class HashMap:
         for bucket in range(self._buckets.length()):
             if self._buckets[bucket] is not None and self._buckets[bucket].key == key:
                 self._buckets[bucket].is_tombstone = True
-
+        self._size -= 1
 
 
 
